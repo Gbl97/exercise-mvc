fcd96df2bfd13a419ef2b82a0cdbbcbb835613e6
Gabriel <g.pereira.a.m@gmail.com>
2021-10-21 19:29:49 -0300

    fix(Exercicio 2) - :sparkle: Salvando o index

--- exercicio-2-piadas-chucknorris/index.js
+++ exercicio-2-piadas-chucknorris/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const categoriesController = require('./controller/categoriesController');
+const jokeController = require('./controller/jokesController');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,5 +13,8 @@ const PORT = 3000;
 
 app.get('/categories', categoriesController.listCategories);
 app.get('/', categoriesController.redirectCategories);
+app.get('/jokes', jokeController.showJoke);
+app.get('/jokes/:category', jokeController.showJokeByCategory);
+
 
 app.listen(PORT, () => console.log('Exercícios do bloco 30 - Chuck Norris parte I'));
